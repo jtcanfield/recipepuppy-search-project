a5259a70d8506e4c8d658db20d746ae88cf1d7f1
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-08 11:35:04 -0400

    add fetch

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-fetch("http://www.recipepuppy.com/api/")
-  .then(\
+fetch("http://recipepuppyproxy.herokuapp.com/api/?q=", {mode: 'cors'})
+  .then(
     function(response) {
       if (response.status !== 200) {
         console.log(response.status);
         return;
       }
       response.json().then(function(data) {
-        console.log("Here is the data:", data);
+        console.log(data);
       });
     }
   )
   .catch(function(err) {
-    console.log("Fetch Error :-S", err);
+    console.log("Fetch Error: ", err);
   });
 function searchSubmit(){
   console.log($("#textinput").val());
