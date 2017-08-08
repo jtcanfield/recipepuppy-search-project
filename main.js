24f308c32fea1a1872aa522b5c038cb1c38cfcd1
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-08 11:15:57 -0400

    testing fetch

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+fetch("http://www.recipepuppy.com/api/")
+  .then(\
+    function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        console.log("Here is the data:", data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log("Fetch Error :-S", err);
+  });
 function searchSubmit(){
   console.log($("#textinput").val());
 }
