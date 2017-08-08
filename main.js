12a489dfbe9e8990e530d33f964b7a34b0f15f8e
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-08 11:49:41 -0400

    add input search function

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-fetch("http://recipepuppyproxy.herokuapp.com/api/?q=", {mode: 'cors'})
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log(response.status);
-        return;
-      }
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log("Fetch Error: ", err);
-  });
 function searchSubmit(){
   console.log($("#textinput").val());
+  fetch("http://recipepuppyproxy.herokuapp.com/api/?q=")
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          for (let i = 0; i < 10; i++)
+          console.log(data.results[i]);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log("Fetch Error: ", err);
+    });
 }
